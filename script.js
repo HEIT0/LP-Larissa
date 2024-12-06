78a5dad178e8c710a72627a23a91fa44ee05441d
HEIT0 <heitorborim@gmail.com>
2024-12-06 18:17:17 -0300

    Versão 1

---
+++ script.js
@@ -0,0 +1,47 @@
+var a = document.getElementById("hid")
+a.style.visibility="hidden"
+function direcionar (){
+  window.location = "ojogo2.html";
+
+}
+function show() {
+  a.style.visibility= "visible"
+  alert("Pegadinha do Silvio...Maoe")
+  var bem = new Audio("bem.m4a")
+  bem.play();
+}
+
+function direcionar1 (){
+  window.location = "azul.html";
+
+}
+function direcionar2 (){
+  window.location = "vermelho.html";
+
+}
+function direcionar3(){
+  audio1 = new Audio("Voz.m4a")
+  audio1.play()
+  
+}
+function direcionar0(){
+  audio1 = new Audio("Voz.m4a")
+  audio1.play()
+  window.location = "final.html";
+  
+}
+audio2 = new Audio("Voz.m4a")
+audio2.play()
+
+function direcionar4 (){
+  window.location = "final.html";
+
+}
+function direcionar5 (){
+  window.location.href = 'https://youtu.be/dQw4w9WgXcQ'
+
+}
+function direcionar6 (){
+  window.location.href = 'https://pt.wikipedia.org/wiki/Rickrolling'
+
+}
